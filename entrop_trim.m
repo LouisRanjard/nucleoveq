4adf46af6a7d65c1685265d5614e88a1c765cf3d
LouisRanjard <louis.ranjard@gmail.com>
2016-10-28 17:22:19 +1100

    find longest contiguous segmetn for trimming on entropy

--- entrop_trim.m
+++ entrop_trim.m
@@ -1,20 +1,25 @@
-function [weight_trimmed ] = entrop_trim(weight)
+function [weight_trimmed ] = entrop_trim(weight,threshold)
 % trim a weight sequence based on the entropy value and the coverage 
 %
 % weight: 4 columns sequence of nucleotides
 % 
 
-    weight_trimmed = weight ;
-
-    t = [0 2 2 3 0 2 3 0 2 2 2 0 2 5 3] ;
+    [ meanEntropy, shannonEnt ] = shannonEntropy(weight) ;
+    
+    if nargin<2
+        threshold=meanEntropy;
+    end
 
     % find the longest contiguous segment
-    x = diff([false t>1 false]);
+    x = diff([false shannonEnt<threshold false]);
     p = find(x==1);
     q = find(x==-1);
-    [maxlen,ix] = max(q-p);
+    %[maxlen,ix] = max(q-p);
+    [~,ix] = max(q-p);
     first = p(ix);
     last = q(ix)-1;
-    fprintf(1,'\nLargest chunk is %d long ([%d-%d])\n',maxlen, first, last);
+    %fprintf(1,'\nLargest chunk is %d long ([%d-%d])\n',maxlen, first, last);
+    
+    weight_trimmed = weight(:,first:last) ;
     
 end
